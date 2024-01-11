8bb704c7a1445d405d690475cda3e6a0c00a507b
shizit <shizit1995@gmail.com>
2024-01-11 17:08:28 +0900

    feat add drawer

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,5 +1,19 @@
+'use client';
 import { memo } from 'react';
+import * as React from 'react';
 import type { FC } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+// import MailIcon from '@mui/icons-material/Mail';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+
 
 import resets from '../_resets.module.css';
 import { ActionButton } from './ActionButton/ActionButton';
@@ -10,8 +24,61 @@ import { Table } from './Table/Table';
 interface Props {
   className?: string;
 }
-/* @figmaId 6:46 */
+
 export default function Home() {
+  const [state, setState] = React.useState({
+    drawerOpen: false
+  });
+
+  const toggleDrawer =
+    (open: boolean) =>
+      (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+          event.type === 'keydown' &&
+          ((event as React.KeyboardEvent).key === 'Tab' ||
+            (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+          return;
+        }
+
+        setState({ ...state, drawerOpen: open });
+      };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  /* @figmaId 6:46 */
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
       <HeaderComponent />
@@ -21,16 +88,28 @@ export default function Home() {
           classes={{ button: classes.button }}
           text={{
             buttonText: <div className={classes.buttonText}>Edit</div>,
-          }}
-        />
+          }} />
         <ActionButton
           classes={{ button: classes.button2 }}
           text={{
             buttonText: <div className={classes.buttonText2}>Delete</div>,
-          }}
-        />
+          }} />
       </div>
       <Table />
+      <div>
+        <React.Fragment>
+          <div className={classes.drawerButton}>
+            <Button onClick={toggleDrawer(true)}>Drawer</Button>
+          </div>
+          <Drawer
+            anchor='right'
+            open={state.drawerOpen}
+            onClose={toggleDrawer(false)}
+          >
+            {list()}
+          </Drawer>
+        </React.Fragment>
+      </div>
     </div>
   );
 };
---
+++ app/test/page.tsx
@@ -0,0 +1,94 @@
+'use client';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function Home() {
+    const [state, setState] = React.useState({
+        drawerOpen: false
+    });
+
+    const toggleDrawer =
+        (open: boolean) =>
+            (event: React.KeyboardEvent | React.MouseEvent) => {
+                if (
+                    event.type === 'keydown' &&
+                    ((event as React.KeyboardEvent).key === 'Tab' ||
+                        (event as React.KeyboardEvent).key === 'Shift')
+                ) {
+                    return;
+                }
+
+                setState({ ...state, drawerOpen: open });
+            };
+    // const toggleDrawer =
+    //     (anchor: Anchor, open: boolean) =>
+    //         (event: React.KeyboardEvent | React.MouseEvent) => {
+    //             if (
+    //                 event.type === 'keydown' &&
+    //                 ((event as React.KeyboardEvent).key === 'Tab' ||
+    //                     (event as React.KeyboardEvent).key === 'Shift')
+    //             ) {
+    //                 return;
+    //             }
+
+    //             setState({ ...state, [anchor]: open });
+    //         };
+
+    const list = () => (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    return (
+        <div>
+            <React.Fragment>
+                <Button onClick={toggleDrawer(true)}>LEFT</Button>
+                <Drawer
+                    anchor='right'
+                    open={state.drawerOpen}
+                    onClose={toggleDrawer(false)}
+                >
+                    {list()}
+                </Drawer>
+            </React.Fragment>
+        </div>
+    );
+}
